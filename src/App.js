53b58a09ed1c0d42f575e8956d4560679dae4ffc
Nguyen Truong Sang <ntsang0705@gmail.com>
2022-12-04 22:18:31 +0700

    update 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { createBrowserHistory } from "history";
-import { Route, Router, Switch } from "react-router-dom";
+import { HashRouter, Route, Router, Switch } from "react-router-dom";
 import { HomeTemplate } from "./templates/HomeTemplate/HomeTemplate";
 import CheckOutTemplate from "./templates/CheckOutTemplate/CheckOutTemplate";
 import Home from "./pages/Home/Home";
@@ -24,29 +24,31 @@ import EditUser from "./pages/Admin/Users/Edit/EditUser";
 export const history = createBrowserHistory();
 export default function App() {
 	return (
-		<Router>
-			<Loading />
-			<Switch>
-				<HomeTemplate exact path="https://ntsang0705.github.io/Booking-cinema-ticket/#/" Component={Home} />
-				<HomeTemplate exact path="/home" Component={Home} />
-				<HomeTemplate exact path="/contact" Component={Contact} />
-				<HomeTemplate exact path="/news" Component={News} />
-				<HomeTemplate exact path="/detail/:id" Component={Detail} />
-				<HomeTemplate exact path="/profile" Component={Profile} />
+		<HashRouter>
+			<Router history={history}>
+				<Loading />
+				<Switch>
+					<HomeTemplate exact path="/" Component={Home} />
+					<HomeTemplate exact path="/home" Component={Home} />
+					<HomeTemplate exact path="/contact" Component={Contact} />
+					<HomeTemplate exact path="/news" Component={News} />
+					<HomeTemplate exact path="/detail/:id" Component={Detail} />
+					<HomeTemplate exact path="/profile" Component={Profile} />
 
-				<Route exact path="/login" component={Login} />
+					<Route exact path="/login" component={Login} />
 
-				<CheckOutTemplate exact path="/checkout/:id" Component={Checkout} />
+					<CheckOutTemplate exact path="/checkout/:id" Component={Checkout} />
 
-				<AdminTemplate exact path="/admin" Component={Dashboard} />
-				<AdminTemplate exact path="/admin/films" Component={Films} />
-				<AdminTemplate exact path="/admin/films/addfilm" Component={Addnew} />
-				<AdminTemplate exact path="/admin/films/edit/:id" Component={Edit} />
-				<AdminTemplate exact path="/admin/films/showtime/:id" Component={ShowTime} />
-				<AdminTemplate exact path="/admin/users" Component={Users} />
-				<AdminTemplate exact path="/admin/users/adduser" Component={Adduser} />
-				<AdminTemplate exact path="/admin/users/edit/:id" Component={EditUser} />
-			</Switch>
-		</Router>
+					<AdminTemplate exact path="/admin" Component={Dashboard} />
+					<AdminTemplate exact path="/admin/films" Component={Films} />
+					<AdminTemplate exact path="/admin/films/addfilm" Component={Addnew} />
+					<AdminTemplate exact path="/admin/films/edit/:id" Component={Edit} />
+					<AdminTemplate exact path="/admin/films/showtime/:id" Component={ShowTime} />
+					<AdminTemplate exact path="/admin/users" Component={Users} />
+					<AdminTemplate exact path="/admin/users/adduser" Component={Adduser} />
+					<AdminTemplate exact path="/admin/users/edit/:id" Component={EditUser} />
+				</Switch>
+			</Router>
+		</HashRouter>
 	);
 }
